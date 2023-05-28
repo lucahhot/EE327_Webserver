e2d84614dd3aff041edfe27c46ae275d290c72f5
Damien <damienkoh2025@u.northwestern.edu>
2023-05-28 05:44:12 -0500

    seperated the joint_tracking functionality

---
+++ src/joint_tracking.cpp
@@ -0,0 +1,126 @@
+#include <arduino.h>
+#include <joint_tracking.h>
+
+String Joint_name_list[number_of_joints] = {"index_finger", "middle_finger"}; // variable names of each joint
+uint8_t Joint_GPIO_list[number_of_joints] = {32, 33}; // GPIO number measuring the resistance of each joint
+Button b_calibrate = {b_calibrate_GPIO, false};
+
+Joint Joint_list[number_of_joints];
+
+void init_joints()
+{
+  //set the resolution to 12 bits (0-4096)
+  analogReadResolution(12);
+  
+  // initializing each Joint object
+  for (uint8_t i = 0; i < number_of_joints; i++){
+      Joint temp_joint = {Joint_name_list[i], Joint_GPIO_list[i], 1000, 0};
+      Joint_list[i] = temp_joint;
+  }
+
+  // setting the pin modes
+  pinMode(LED_calibrate_GPIO, OUTPUT);
+  pinMode(b_calibrate_GPIO, INPUT_PULLUP);
+  attachInterrupt(b_calibrate_GPIO, isr, FALLING);
+  for (uint8_t i = 0; i < number_of_joints; i++){
+      pinMode(Joint_GPIO_list[i], INPUT);
+  }
+
+  // setting the initial states
+  digitalWrite(LED_calibrate_GPIO, LOW);
+}
+
+void ARDUINO_ISR_ATTR isr()
+{
+    b_calibrate.pressed = true;
+}
+
+void LED_1flash(uint8_t PIN)
+{
+    digitalWrite(PIN, LOW);
+    delay(500);
+    digitalWrite(PIN, HIGH);
+    delay(500);
+}
+
+void update_Joint_res_opened(uint32_t* data)
+{
+    for(uint8_t i = 0; i < number_of_joints; i++) {
+        Joint_list[i].res_opened = data[i];
+    }
+}
+
+void update_Joint_res_closed(uint32_t* data)
+{
+    for(uint8_t i = 0; i < number_of_joints; i++) {
+        Joint_list[i].res_closed = data[i];
+    }
+}
+
+void calibrate_joints()
+{
+    digitalWrite(LED_calibrate_GPIO, HIGH);
+    Serial.printf("Open hand 1/3\n");
+    for (uint8_t i = 0; i < 3; i++){
+      LED_1flash(LED_calibrate_GPIO);
+    }
+
+    delay(2500); //TAKE THE OPEN HAND READING
+    uint32_t* open_data = find_res(Joint_GPIO_list, number_of_joints);
+    update_Joint_res_opened(open_data);
+    delete[] open_data;
+
+    Serial.printf("Close hand 2/3\n");
+    for (uint8_t i = 0; i < 2; i++){
+      LED_1flash(LED_calibrate_GPIO);
+    }
+    
+    delay(2500); //TAKE THE CLOSED HAND READING
+    uint32_t* closed_data = find_res(Joint_GPIO_list, number_of_joints);
+    update_Joint_res_closed(closed_data);
+    delete[] closed_data;
+
+    LED_1flash(LED_calibrate_GPIO);
+    Serial.printf("Relax hand 3/3\n");
+    Serial.printf("Calibration complete\n");
+    digitalWrite(LED_calibrate_GPIO, LOW);
+}
+
+uint32_t* find_res(uint8_t* pins, uint8_t numPins)
+{
+    
+    uint32_t* GPIOValues = new uint32_t[numPins];
+    for (uint8_t i = 0; i < numPins; i++) {
+      GPIOValues[i] = analogReadMilliVolts(pins[i]);
+    }
+    
+    uint32_t* res_values = new uint32_t[numPins];
+  
+    for (uint8_t i = 0; i < numPins; i++) {
+      uint32_t val = GPIOValues[i];
+      uint32_t res_val = known_R*(Vin/(val/1000.0) - 1.0);
+      res_values[i] = res_val;
+    }
+
+    delete[] GPIOValues;
+
+    return res_values;
+}
+
+uint8_t* finger_joint_state(uint32_t* data)
+{
+    uint8_t* state_list = new uint8_t[number_of_joints];
+    
+    for(uint8_t i = 0; i < number_of_joints; i++){
+        int32_t open_diff = data[i] - Joint_list[i].res_opened;
+        int32_t close_diff = data[i] - Joint_list[i].res_closed;
+        if(abs(close_diff) <= abs(open_diff)){
+            state_list[i] = 0; // Joint is closed
+        }
+        else{
+            state_list[i] = 1; //Joint is opened
+        }
+    }
+
+    return state_list;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,14 @@
 *********/
 
 #include <Arduino.h>
+
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <joint_tracking.h>
+
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
 #include <Arduino_JSON.h>
 #include <esp_adc_cal.h>
 #include "SPIFFS.h"
@@ -137,191 +140,6 @@ String getTemperature(){
   return String(temperature);
 }
 
-//
-// OPEN TO CHANGE FOR USER
-//
-
-#define number_of_joints      2 // number of joints being measured
-String Joint_name_list[number_of_joints] = {"index_finger", "middle_finger"}; // variable names of each joint
-uint8_t Joint_GPIO_list[number_of_joints] = {32, 33}; // GPIO number measuring the resistance of each joint
-
-//
-// STRUCTS
-//
-
-struct Joint {
-    String name;
-    uint8_t GPIO;
-    uint32_t res_opened;
-    uint32_t res_closed;
-};
-
-struct Button {
-    const uint8_t GPIO;
-    bool pressed;
-};
-
-//
-// INITIALIZATION
-//
-
-Joint Joint_list[number_of_joints];
-
-//
-// PIN NUMBER DECLARATION
-//
-
-#define LED_calibrate_GPIO     19
-#define b_calibrate_GPIO       18
-
-//
-// RAW DATA / adcPIN READING
-//
-
-/*
-uint8_t* readGPIOs(uint8_t* pins, uint8_t numPins) {
-  uint8_t* GPIOValues = new uint8_t[numPins];
-  for (int i = 0; i < numPins; i++) {
-    GPIOValues[i] = analogReadMilliVolts(pins[i]);
-  }
-  return GPIOValues;
-}
-*/
-
-//
-// CALIBRATE SETUP
-//
-
-Button b_calibrate = {b_calibrate_GPIO, false};
-
-void ARDUINO_ISR_ATTR isr() {
-    b_calibrate.pressed = true;
-}
-
-void LED_1flash(uint8_t PIN){
-    digitalWrite(PIN, LOW);
-    delay(500);
-    digitalWrite(PIN, HIGH);
-    delay(500);
-}
-
-void update_Joint_res_opened(uint32_t* data) {
-    for(uint8_t i = 0; i < number_of_joints; i++) {
-        Joint_list[i].res_opened = data[i];
-    }
-}
-
-void update_Joint_res_closed(uint32_t* data) {
-    for(uint8_t i = 0; i < number_of_joints; i++) {
-        Joint_list[i].res_closed = data[i];
-    }
-}
-
-//
-// RESISTANCE READING SETUP
-//
-
-float Vin = 3.3;
-float known_R = 2200.0;
-
-uint32_t* find_res(uint8_t* pins, uint8_t numPins){
-    
-    uint32_t* GPIOValues = new uint32_t[numPins];
-    for (uint8_t i = 0; i < numPins; i++) {
-      GPIOValues[i] = analogReadMilliVolts(pins[i]);
-    }
-    
-    uint32_t* res_values = new uint32_t[numPins];
-  
-    for (uint8_t i = 0; i < numPins; i++) {
-      uint32_t val = GPIOValues[i];
-      uint32_t res_val = known_R*(Vin/(val/1000.0) - 1.0);
-      res_values[i] = res_val;
-    }
-
-    delete[] GPIOValues;
-
-    return res_values;
-}
-
-void calibrate_joints(){
-    digitalWrite(LED_calibrate_GPIO, HIGH);
-    Serial.printf("Open hand 1/3\n");
-    for (uint8_t i = 0; i < 3; i++){
-      LED_1flash(LED_calibrate_GPIO);
-    }
-
-    delay(2500); //TAKE THE OPEN HAND READING
-    uint32_t* open_data = find_res(Joint_GPIO_list, number_of_joints);
-    update_Joint_res_opened(open_data);
-    delete[] open_data;
-
-    Serial.printf("Close hand 2/3\n");
-    for (uint8_t i = 0; i < 2; i++){
-      LED_1flash(LED_calibrate_GPIO);
-    }
-    
-    delay(2500); //TAKE THE CLOSED HAND READING
-    uint32_t* closed_data = find_res(Joint_GPIO_list, number_of_joints);
-    update_Joint_res_closed(closed_data);
-    delete[] closed_data;
-
-    LED_1flash(LED_calibrate_GPIO);
-    Serial.printf("Relax hand 3/3\n");
-    Serial.printf("Calibration complete\n");
-    digitalWrite(LED_calibrate_GPIO, LOW);
-}
-
-//
-// HAND_STATE SETUP
-//
-template <typename T>
-void print_list(String label, T list, uint8_t numPins) {
-    Serial.println(label);
-    for (int i = 0; i < numPins; i++) {
-      Serial.print(list[i]);
-      if(i != (numPins - 1)){
-        Serial.print(", ");
-      }
-    }
-    Serial.println("");
-}
-
-uint8_t* finger_joint_state(uint32_t* data){
-    uint8_t* state_list = new uint8_t[number_of_joints];
-    
-    for(uint8_t i = 0; i < number_of_joints; i++){
-        int32_t open_diff = data[i] - Joint_list[i].res_opened;
-        int32_t close_diff = data[i] - Joint_list[i].res_closed;
-        if(abs(close_diff) <= abs(open_diff)){
-            state_list[i] = 0; // Joint is closed
-        }
-        else{
-            state_list[i] = 1; //Joint is opened
-        }
-    }
-
-    return state_list;
-}
-
-void init_joints(){
-  // initializing each Joint object
-  for (uint8_t i = 0; i < number_of_joints; i++){
-      Joint temp_joint = {Joint_name_list[i], Joint_GPIO_list[i], 1000, 0};
-      Joint_list[i] = temp_joint;
-  }
-
-  // setting the pin modes
-  pinMode(LED_calibrate_GPIO, OUTPUT);
-  pinMode(b_calibrate_GPIO, INPUT_PULLUP);
-  attachInterrupt(b_calibrate_GPIO, isr, FALLING);
-  for (uint8_t i = 0; i < number_of_joints; i++){
-      pinMode(Joint_GPIO_list[i], INPUT);
-  }
-
-  // setting the initial states
-  digitalWrite(LED_calibrate_GPIO, LOW);
-}
 
 void setup() {
   Serial.begin(115200);
@@ -329,8 +147,6 @@ void setup() {
   initSPIFFS();
   initMPU();
 
-  //set the resolution to 12 bits (0-4096)
-  analogReadResolution(12);
   init_joints();
 
   // Handle Web Server
@@ -398,7 +214,6 @@ void loop() {
   delay(50);
 
   print_list<String[number_of_joints]>("Joint names:", Joint_name_list, number_of_joints);
-  // print_list<uint32_t[number_of_joints]>("Resistance values:", calculated_data, number_of_joints);
   print_list<uint8_t[number_of_joints]>("Joint states:", joint_states, number_of_joints);
 
   if ((millis() - lastFingerButton) > fingerDelay) {
