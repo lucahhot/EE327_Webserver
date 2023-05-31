a49b1f767b711c41c9c1dc80a73e7886b1a36f0b
Damien <damienkoh2025@u.northwestern.edu>
2023-05-31 15:38:30 -0500

    integrated gesture recognition into existing codebase

--- src/gesture_recognition.cpp
+++ src/gesture_recognition.cpp
@@ -1,108 +1,108 @@
-#include <Arduino.h>
-#include <gesture_recognition.h>
+// #include <Arduino.h>
+// #include <gesture_recognition.h>
 
-Button b_gesture = {b_gesture_GPIO, false};
-float features[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE];
-size_t feature_ix = 0;
-unsigned long last_interval_ms = 0;
-bool gesture_recognized;
+// Button b_gesture = {b_gesture_GPIO, false};
+// float features[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE];
+// size_t feature_ix = 0;
+// unsigned long last_interval_ms = 0;
+// bool gesture_recognized;
 
-extern Adafruit_MPU6050 mpu;
+// extern Adafruit_MPU6050 mpu;
 
-void init_gesture()
-{
-  pinMode(LED_gesture_GPIO, OUTPUT);
-  pinMode(b_gesture_GPIO, INPUT_PULLUP);
-  attachInterrupt(b_gesture_GPIO, isr_gesture, FALLING);
+// void init_gesture()
+// {
+//   pinMode(LED_gesture_GPIO, OUTPUT);
+//   pinMode(b_gesture_GPIO, INPUT_PULLUP);
+//   attachInterrupt(b_gesture_GPIO, isr_gesture, FALLING);
 
-  Serial.print("Features: ");
-  Serial.println(EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE);
-  Serial.print("Label count: ");
-  Serial.println(EI_CLASSIFIER_LABEL_COUNT);
+//   Serial.print("Features: ");
+//   Serial.println(EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE);
+//   Serial.print("Label count: ");
+//   Serial.println(EI_CLASSIFIER_LABEL_COUNT);
 
-}
+// }
 
-void ARDUINO_ISR_ATTR isr_gesture()
-{
-    b_gesture.pressed = true;
-}
+// void ARDUINO_ISR_ATTR isr_gesture()
+// {
+//     b_gesture.pressed = true;
+// }
 
-void gesture_recognition_loop()
-{
-    if(b_gesture.pressed){
-    digitalWrite(LED_gesture_GPIO, HIGH);
-    sensors_event_t a, g, temp;
+// void gesture_recognition_loop()
+// {
+//     if(b_gesture.pressed){
+//     digitalWrite(LED_gesture_GPIO, HIGH);
+//     sensors_event_t a, g, temp;
 
-    if (millis() > last_interval_ms + INTERVAL_MS) {
-      last_interval_ms = millis();
+//     if (millis() > last_interval_ms + INTERVAL_MS) {
+//       last_interval_ms = millis();
 
-      mpu.getEvent(&a, &g, &temp);
+//       mpu.getEvent(&a, &g, &temp);
 
-      features[feature_ix++] = a.acceleration.x;
-      features[feature_ix++] = a.acceleration.y;
-      features[feature_ix++] = a.acceleration.z;
-      features[feature_ix++] = g.gyro.x;
-      features[feature_ix++] = g.gyro.y;
-      features[feature_ix++] = g.gyro.z;
+//       features[feature_ix++] = a.acceleration.x;
+//       features[feature_ix++] = a.acceleration.y;
+//       features[feature_ix++] = a.acceleration.z;
+//       features[feature_ix++] = g.gyro.x;
+//       features[feature_ix++] = g.gyro.y;
+//       features[feature_ix++] = g.gyro.z;
 
-      if (feature_ix == EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE) {
-        Serial.println("Running the inference...");
-        signal_t signal;
-        ei_impulse_result_t result;
-        int err = numpy::signal_from_buffer(features, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, &signal);
-        if (err != 0) {
-          ei_printf("Failed to create signal from buffer (%d)\n", err);
-          return;
-        }
+//       if (feature_ix == EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE) {
+//         Serial.println("Running the inference...");
+//         signal_t signal;
+//         ei_impulse_result_t result;
+//         int err = numpy::signal_from_buffer(features, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, &signal);
+//         if (err != 0) {
+//           ei_printf("Failed to create signal from buffer (%d)\n", err);
+//           return;
+//         }
 
-        EI_IMPULSE_ERROR res = run_classifier(&signal, &result, true);
+//         EI_IMPULSE_ERROR res = run_classifier(&signal, &result, true);
 
-        if (res != 0) return;
+//         if (res != 0) return;
 
-        ei_printf("Predictions ");
-        ei_printf("(DSP: %d ms., Classification: %d ms.)",
-                  result.timing.dsp, result.timing.classification);
-        ei_printf(": \n");
+//         ei_printf("Predictions ");
+//         ei_printf("(DSP: %d ms., Classification: %d ms.)",
+//                   result.timing.dsp, result.timing.classification);
+//         ei_printf(": \n");
 
-        gesture_recognized = false;
-        for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
-          ei_printf("    %s: %.5f\n", result.classification[ix].label, result.classification[ix].value);
-          if (result.classification[ix].value > 0.6) {
-            gesture_recognized = true;
-            if (result.classification[ix].label == "left_to_right")
-            {
-              ei_printf("Hand moved from left to right.\n");
-            } else if (result.classification[ix].label == "right_to_left")
-            {
-              ei_printf("Hand moved from right to left.\n");
-            }
-          }
-        }
+//         gesture_recognized = false;
+//         for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+//           ei_printf("    %s: %.5f\n", result.classification[ix].label, result.classification[ix].value);
+//           if (result.classification[ix].value > 0.6) {
+//             gesture_recognized = true;
+//             if (result.classification[ix].label == "left_to_right")
+//             {
+//               ei_printf("Hand moved from left to right.\n");
+//             } else if (result.classification[ix].label == "right_to_left")
+//             {
+//               ei_printf("Hand moved from right to left.\n");
+//             }
+//           }
+//         }
 
-        if(!gesture_recognized){
-          ei_printf("Unable to identify gesture.\n");
-        }
+//         if(!gesture_recognized){
+//           ei_printf("Unable to identify gesture.\n");
+//         }
 
-        b_gesture.pressed = false;
-        digitalWrite(LED_gesture_GPIO, LOW);
+//         b_gesture.pressed = false;
+//         digitalWrite(LED_gesture_GPIO, LOW);
 
-        feature_ix = 0;
-      }
+//         feature_ix = 0;
+//       }
 
-    }
-  }
-}
+//     }
+//   }
+// }
 
-void ei_printf(const char *format, ...)
-{
-  static char print_buf[1024] = { 0 };
+// void ei_printf(const char *format, ...)
+// {
+//   static char print_buf[1024] = { 0 };
 
-  va_list args;
-  va_start(args, format);
-  int r = vsnprintf(print_buf, sizeof(print_buf), format, args);
-  va_end(args);
+//   va_list args;
+//   va_start(args, format);
+//   int r = vsnprintf(print_buf, sizeof(print_buf), format, args);
+//   va_end(args);
 
-  if (r > 0) {
-    Serial.write(print_buf);
-  }
-}
+//   if (r > 0) {
+//     Serial.write(print_buf);
+//   }
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -23,8 +23,129 @@
 #include <Adafruit_Sensor.h>
 #include <joint_tracking.h>
 
-#include <gesture_recognition.h>
+//
+// GESTURE RECOGNITION FUNCTIONS
+//
 
+// For whatever reason, placing #include "gesture_recognition_v1_inferencing.h" in a file that's not main.cpp causes
+// "multiple definition of..." errors, making creating seperate .cpp and .h files for the relevant functions very difficult.
+#include "gesture_recognition_v1_inferencing.h"
+
+#define b_gesture_GPIO      17
+#define LED_gesture_GPIO    5
+#define FREQUENCY_HZ        60
+#define INTERVAL_MS         (1000 / (FREQUENCY_HZ + 1))
+
+Button b_gesture = {b_gesture_GPIO, false};
+float features[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE];
+size_t feature_ix = 0;
+unsigned long last_interval_ms = 0;
+bool gesture_recognized;
+
+extern Adafruit_MPU6050 mpu;
+
+void ARDUINO_ISR_ATTR isr_gesture()
+{
+    b_gesture.pressed = true;
+}
+
+void init_gesture()
+{
+  pinMode(LED_gesture_GPIO, OUTPUT);
+  pinMode(b_gesture_GPIO, INPUT_PULLUP);
+  attachInterrupt(b_gesture_GPIO, isr_gesture, FALLING);
+
+  Serial.print("Features: ");
+  Serial.println(EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE);
+  Serial.print("Label count: ");
+  Serial.println(EI_CLASSIFIER_LABEL_COUNT);
+
+}
+
+void gesture_recognition_loop()
+{
+    if(b_gesture.pressed){
+    digitalWrite(LED_gesture_GPIO, HIGH);
+    sensors_event_t a, g, temp;
+
+    if (millis() > last_interval_ms + INTERVAL_MS) {
+      last_interval_ms = millis();
+
+      mpu.getEvent(&a, &g, &temp);
+
+      features[feature_ix++] = a.acceleration.x;
+      features[feature_ix++] = a.acceleration.y;
+      features[feature_ix++] = a.acceleration.z;
+      features[feature_ix++] = g.gyro.x;
+      features[feature_ix++] = g.gyro.y;
+      features[feature_ix++] = g.gyro.z;
+
+      if (feature_ix == EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE) {
+        Serial.println("Running the inference...");
+        signal_t signal;
+        ei_impulse_result_t result;
+        int err = numpy::signal_from_buffer(features, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, &signal);
+        if (err != 0) {
+          ei_printf("Failed to create signal from buffer (%d)\n", err);
+          return;
+        }
+
+        EI_IMPULSE_ERROR res = run_classifier(&signal, &result, true);
+
+        if (res != 0) return;
+
+        ei_printf("Predictions ");
+        ei_printf("(DSP: %d ms., Classification: %d ms.)",
+                  result.timing.dsp, result.timing.classification);
+        ei_printf(": \n");
+
+        gesture_recognized = false;
+        for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+          ei_printf("    %s: %.5f\n", result.classification[ix].label, result.classification[ix].value);
+          if (result.classification[ix].value > 0.6) {
+            gesture_recognized = true;
+            if (result.classification[ix].label == "left_to_right")
+            {
+              ei_printf("Hand moved from left to right.\n");
+            } else if (result.classification[ix].label == "right_to_left")
+            {
+              ei_printf("Hand moved from right to left.\n");
+            }
+          }
+        }
+
+        if(!gesture_recognized){
+          ei_printf("Unable to identify gesture.\n");
+        }
+
+        b_gesture.pressed = false;
+        digitalWrite(LED_gesture_GPIO, LOW);
+
+        feature_ix = 0;
+      }
+
+    }
+  }
+}
+
+void ei_printf(const char *format, ...)
+{
+  static char print_buf[1024] = { 0 };
+
+  va_list args;
+  va_start(args, format);
+  int r = vsnprintf(print_buf, sizeof(print_buf), format, args);
+  va_end(args);
+
+  if (r > 0) {
+    Serial.write(print_buf);
+  }
+}
+
+
+//
+// MAIN CODE
+//
 
 
 void setup() {
@@ -38,7 +159,7 @@ void setup() {
 
   init_joints();
 
-  //init_gesture();
+  init_gesture();
 }
 
 void loop() {
@@ -62,7 +183,7 @@ void loop() {
       b_calibrate.pressed = false;
   }
 
-  //gesture_recognition_loop();
+  gesture_recognition_loop();
 
-  delay(10);
+  delay(50);
 }
